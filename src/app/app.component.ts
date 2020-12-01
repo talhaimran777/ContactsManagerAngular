b7d44ba6d67a2ac2294f2b9a38e822b484b283ba
Talha Imran <talhaimran284@gmail.com>
2020-12-01 15:09:14 +0500

    Used ngStyle to add styles dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'contacts-manager-angular';
+  currentStyles: any;
+  makeTitleRed = true;
+
+  constructor() {
+    this.currentStyles = {
+      color: this.makeTitleRed === true ? 'red' : 'green',
+    };
+  }
 }
