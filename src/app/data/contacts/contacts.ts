e6c7bf8549e5dc8f215df849e55feafcf16b87d9
Talha Imran <talhaimran284@gmail.com>
2020-12-01 17:44:56 +0500

    Added expand and collapse functionality by code

--- src/app/data/contacts/contacts.ts
+++ src/app/data/contacts/contacts.ts
@@ -2,6 +2,7 @@ export interface Contact {
   name: string;
   email: string;
   phone: string;
+  hideDetails: boolean;
 }
 
 export const contacts: Contact[] = [
@@ -9,15 +10,18 @@ export const contacts: Contact[] = [
     name: 'Talha Imran',
     email: 'talhaimran284@gmail.com',
     phone: '555-555-5555',
+    hideDetails: false,
   },
   {
     name: 'Hamza Imran',
     email: 'hamzaimran284@gmail.com',
     phone: '555-555-5555',
+    hideDetails: true,
   },
   {
     name: 'Hassaan Farouqui',
     email: 'hfarouqui3284@gmail.com',
     phone: '555-555-5555',
+    hideDetails: true,
   },
 ];
